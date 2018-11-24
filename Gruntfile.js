7e44b736c1f7062443ca6a0f665904789b11ad91
Mickaël Floc’hlay <mickael.flochlay@margo.com>
2018-11-23 19:11:28 -0500

    style: switch to SASS and Bootstrap 4.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,6 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    less: {
-      production: {
-        options: {
-          paths: ["node_modules/bootstrap/less"],
-          yuicompress: true
-        },
-        files: {
-          "assets/css/application.min.css": "assets/_less/application.less"
-        }
-      }
-    },
     uglify: {
       jquery: {
         files: {
@@ -20,11 +9,11 @@ module.exports = function(grunt) {
       },
       bootstrap: {
         files: {
-          'assets/js/bootstrap.min.js': ['node_modules/bootstrap/js/collapse.js',
-            'node_modules/bootstrap/js/scrollspy.js',
-            'node_modules/bootstrap/js/tooltip.js',
-            'node_modules/bootstrap/js/button.js',
-            'node_modules/bootstrap/js/affix.js'
+          'assets/js/bootstrap.min.js': ['node_modules/bootstrap/js/dist/collapse.js',
+            'node_modules/bootstrap/js/dist/scrollspy.js',
+            'node_modules/bootstrap/js/dist/tooltip.js',
+            'node_modules/bootstrap/js/dist/button.js',
+            'node_modules/bootstrap/js/dist/affix.js'
           ]
         }
       },
@@ -49,29 +38,6 @@ module.exports = function(grunt) {
             dest: 'assets/vendors/fontawesome/webfonts/'
           }
         ]
-      },
-      littlefoot: {
-        files: [{
-            expand: true,
-            cwd: 'node_modules/littlefoot/dist/',
-            src: ['littlefoot.min.js'],
-            dest: 'assets/js/'
-          },
-          {
-            expand: true,
-            cwd: 'node_modules/littlefoot/dist/',
-            src: ['littlefoot.css'],
-            dest: 'assets/css/'
-          },
-        ]
-      },
-      requirejs: {
-        files: [{
-          expand: true,
-          cwd: 'node_modules/requirejs/',
-          src: ['require.js'],
-          dest: 'assets/vendors/requirejs/'
-        }]
       }
     },
     coffee: {
@@ -80,25 +46,11 @@ module.exports = function(grunt) {
           'assets/_javascript/deadrooster.js': 'assets/_coffeescript/deadrooster.js.coffee'
         }
       }
-    },
-    exec: {
-      build: {
-        cmd: 'jekyll build'
-      },
-      serve: {
-        cmd: 'jekyll serve --watch'
-      }
-    }
-  });
+    }  });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask('javascript', ['coffee', 'uglify'])
-  grunt.registerTask('default', ['less', 'coffee', 'uglify', 'copy', 'exec:build']);
-  grunt.registerTask('deploy', ['default', 'exec:deploy']);
-
+  grunt.registerTask('default', ['coffee', 'uglify', 'copy' ]);
 };
